ffa3cef613450680362d2c0496cc2703d2697cc4
Joshua Kaden <jkaden@kadenspace.com>
2014-02-12 15:36:25 -0500

    Initial Commit

---
+++ Circulate/JSKViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSKViewController.h
+//  Circulate
+//
+//  Created by Joshua Kaden on 2/12/14.
+//  Copyright (c) 2014 Chadford Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSKViewController : UIViewController
+
+@end
---
+++ Circulate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Circulate
+//
+//  Created by Joshua Kaden on 2/12/14.
+//  Copyright (c) 2014 Chadford Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSKAppDelegate class]));
+    }
+}
